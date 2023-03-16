cd7081edd34b4037b2f48442c2b58d9972a6d0b5
Ather_Weather <marcelstaniszewski@gmx.de>
2023-03-16 11:13:04 +0100

    Zombie added , attack order added , zombie roation and movemnt to order added

---
+++ ZombieEvolution/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public List<GameObject> zombies;
+
+    public Camera sceneCamera;
+
+    public float energy;
+    public float energyCap;
+    public float energyRegeneration;
+
+    public GameObject attackorder;
+    private Vector3 target;
+
+    public Vector2 mousePosition;
+
+    public float attackOrderCost;
+
+    void Start ()
+    {
+       Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (energy >= attackOrderCost && Input.GetMouseButtonDown(0))
+        {
+            energy -= attackOrderCost;      
+            for (int i = 0; i < zombies.Count; i++)
+            {
+                zombies[i].GetComponent<Zombie>().setTarget(mousePosition);
+            }
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        energy = Mathf.Min(energyCap, energy + energyRegeneration);
+        target = sceneCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y , sceneCamera.transform.position.z));
+        attackorder.transform.position = new Vector2(target.x, target.y);
+        mousePosition = new Vector2(target.x, target.y);
+    }
+}
---
+++ ZombieEvolution/Assets/Scripts/Zombie.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float movementspeed;
+    public float turnspeed;
+    public Rigidbody2D rigidbody;
+    public Vector2 target;
+    public Vector2 moveDirection;
+    public Vector2 directionVector;
+    private bool targetreached;
+    public float snappingnumber;
+
+    void Start()
+    {
+        targetreached = true;
+        snappingnumber = 0.3f;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProccessOrder();
+    }
+
+    void FixedUpdate()
+    {
+        if (!targetreached)
+        {
+            Move();
+            if (directionVector.magnitude  < snappingnumber)
+            {
+                targetreached = true;                
+                rigidbody.velocity = new Vector2(0, 0);
+            }
+            
+
+        }
+    }
+
+    void ProccessOrder()
+    {
+        directionVector = new Vector2(target.x - rigidbody.position.x, target.y - rigidbody.position.y);
+        moveDirection = directionVector;
+        moveDirection = moveDirection.normalized;
+    }
+
+    void Move()
+    {
+        rigidbody.velocity = new Vector2(moveDirection.x * movementspeed, moveDirection.y * movementspeed);
+        float aimAngle = Mathf.Atan2(directionVector.y, directionVector.x) * Mathf.Rad2Deg - 90f;
+        rigidbody.rotation = aimAngle;
+    }
+
+    public void setTarget(Vector2 newTarget)
+    {
+        target = newTarget;
+        targetreached = false;
+    }
+}
